257c305dd6bfb2e8db8a0c5148b9cfc1441e2731
ivvlko <ikoev@vikta.com>
2025-11-22 11:54:37 +0200

    dont run monitoring service on build and set top k based on report

--- websocket_service/config.py
+++ websocket_service/config.py
@@ -4,6 +4,6 @@ rag_configs = {
     "EMBEDDING_MODEL": os.getenv("EMBEDDING_MODEL", "text-embedding-3-small"),
     "LLM_MODEL": "gpt-4o-mini",
     "OPEN_AI_API_KEY": os.getenv("OPEN_AI_API_KEY"),
-    "TOP_K": 5,
+    "TOP_K": 3,
     "SYSTEM_PROMPT": "Answer the question concisely"
 }
